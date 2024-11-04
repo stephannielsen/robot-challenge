d7f94ee3a9c94aeb0acacd2025fcc3f2025638f9
Stephan Nielsen <stephannielsen@gmail.com>
2024-11-04 22:53:52 +0100

    build: Fix some lint warnings

--- RobotService/CleaningPathIsValidFilter.cs
+++ RobotService/CleaningPathIsValidFilter.cs
@@ -4,15 +4,15 @@ namespace RobotService;
 
 public class CleaningPathIsValidFilter : IEndpointFilter
 {
-    public async ValueTask<object?> InvokeAsync(EndpointFilterInvocationContext efiContext,
+    public async ValueTask<object?> InvokeAsync(EndpointFilterInvocationContext context,
         EndpointFilterDelegate next)
     {
-        var path = efiContext.GetArgument<CleaningPath>(0);
+        var path = context.GetArgument<CleaningPath>(0);
 
         if (!CleaningPath.IsValid(path!, out Dictionary<string, string[]> problems))
         {
             return Results.ValidationProblem(problems);
         }
-        return await next(efiContext);
+        return await next(context);
     }
 }
